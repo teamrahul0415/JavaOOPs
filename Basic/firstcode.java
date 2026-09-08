26c1329dc50c77c367104c6e9ad02890dad8498f
Rahul Kumar <teamrahul0415@gmail.com>
2026-09-08 12:26:35 +0530

    add

--- Basic/firstcode.java
+++ Basic/firstcode.java
@@ -3,6 +3,8 @@
 public class firstcode {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.print("Hello World! Hlo");
+        System.out.println("Team");
     }
     
 }
